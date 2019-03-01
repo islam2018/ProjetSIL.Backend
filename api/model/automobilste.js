700558cf623013663f669114a3609defdaa11162
unknown <fm_bouayache@esi.dz>
2019-03-02 00:12:11 +0100

    Correction des erreurs dans la fonctionalite d'authentification

--- api/model/automobilste.js
+++ api/model/automobilste.js
@@ -1,8 +1,8 @@
 const Sequelize = require('sequelize');
 const sequelize = require('../config/dbconnection');
 
-const Automobiliste = sequelize.define('automobilste', {
-    idAutomobilste: {type:Sequelize.INTEGER,primaryKey: true},
+const Automobiliste = sequelize.define('automobiliste', {
+    idAutomobiliste: {type:Sequelize.INTEGER,primaryKey: true},
     NumTel: {type:Sequelize.STRING},
     Nom: {type:Sequelize.STRING},
     Prenom: {type:Sequelize.STRING}
--- api/routes/auth/automob.js
+++ api/routes/auth/automob.js
@@ -7,16 +7,20 @@ const automobilsteService = new AutomobilisteService();
 router.post('/',AutoMobAccesControl,(req,res) => {
     automobilsteService.getAutomobilste(req.body.idAutomobiliste).then(automob=>{
        if (automob==null) {
-           automobilsteService.createAutomobilste(req.body).then().catch(e=>{
+           automobilsteService.createAutomobilste(req.body).then((value)=>{
+               res.status(200).json({
+                   message: "Authentification réussite !"
+             });}
+           ).catch(e=>{
                res.status(500).json({
                    message: "Une erreur a éte produite !"
                });
            });
-       }
+       } else {
         res.status(200).json({
             message: "Authentification réussite !"
         });
-
+        }
     }).catch(e=>{
         res.status(500).json({
             message: "Une erreur a éte produite !"
