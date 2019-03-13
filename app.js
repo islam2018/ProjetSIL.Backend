e5a1f1d4f846611dbda4969ec04300356363e4dd
islam <fm_bouayache@esi.dz>
2019-03-13 12:38:03 +0100

    correction d'une erreur CORS

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use((req,res,next)=>{
    res.header('Access-Control-Allow-Origin','*');
    res.header('Access-Control-Allow-Headers','X-Requested-With, Origin, Content-Type, Authorization, Form-Data');
-    req.header('Access-Control-Allow-Methods','PUT,POST,GET,DELETE');
+    res.header('Access-Control-Allow-Methods','PUT,POST,GET,DELETE');
    next();
 });
 
