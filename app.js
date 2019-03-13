3d4f033ec8050a50df8c1c000c2785dd446f8f21
islam <fm_bouayache@esi.dz>
2019-03-13 12:21:03 +0100

    Correction d'une erreur de type CORS

--- app.js
+++ app.js
@@ -10,10 +10,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use((req,res,next)=>{
    res.header('Access-Control-Allow-Origin','*');
    res.header('Access-Control-Allow-Headers','Origin, Content-Type, Authorization, Form-Data');
-   if (req.method==='OPTIONS') {
-       req.header('Access-Control-Allow-Methods','PUT,POST,GET,DELETE');
-       return res.status(200).json({});
-   }
+    req.header('Access-Control-Allow-Methods','PUT,POST,GET,DELETE');
    next();
 });
 
