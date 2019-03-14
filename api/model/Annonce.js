e6bd53f81bd3c0417454954d2c77db4882a37af2
islam <fm_bouayache@esi.dz>
2019-03-14 14:11:29 +0100

    upload des images des annonces

--- api/model/Annonce.js
+++ api/model/Annonce.js
@@ -3,7 +3,6 @@ const sequelize = require('../config/dbconnection');
 
 const Annonce = sequelize.define('annonce', {
     idAnnonce: {type:Sequelize.INTEGER,primaryKey: true, autoIncrement: true},
-    CheminPhoto: {type:Sequelize.TEXT},
     Prix: {type:Sequelize.STRING},
     idAutomobiliste: {type:Sequelize.INTEGER},
     CodeVersion : {type:Sequelize.INTEGER}
