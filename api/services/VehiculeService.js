3d520796be0b545abe7e04c25fead8ebc8d0aec7
islam <fm_bouayache@esi.dz>
2019-07-13 15:03:47 +0100

    refactoring

--- api/services/VehiculeService.js
+++ api/services/VehiculeService.js
@@ -15,7 +15,7 @@ let VehiculeService=class VehiculeService {
                 include:[
                     {model:VERSION,as:'version'}
                 ]
-            },{where:{CodeVersion:body.codeVersion,CodeCouleur:body.couleur.codeCouleur}}).then(data=>{
+            },{where:{CodeVersion:body.codeVersion}}).then(data=>{
                 let res = {};
                 if (data.length === 0) {
                     res.disponible = false;
