3ad56495fca5c911231b8a592b75844cd77805d0
islam <fm_bouayache@esi.dz>
2019-07-13 16:12:50 +0100

    refactoring

--- api/routes/vehicules/stock.js
+++ api/routes/vehicules/stock.js
@@ -45,7 +45,7 @@ router.post('/disponible',(req,res)=>{
         res.status(200).json(data);
     }).catch(e=>{
         res.status(500).json({
-            message:'Une erreur s\'est produite'
+            message:'Une erreur s\'est produite'+e
         });
     })
 });
